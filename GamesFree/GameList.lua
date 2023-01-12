9451d5ef4f81b9df88825040bba32b45a593b14c
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2023-01-12 03:57:18 +0100

    Update GameList.lua

--- GamesFree/GameList.lua
+++ GamesFree/GameList.lua
@@ -1,7 +1,7 @@
 getgenv().Games = {
-    [2534724415] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/Viper%20Hub%20Games/ViperHub_SCP.lua", --ERLC
-    [155615604] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/Viper%20Hub%20Games/ViperHub_VehicleSimulator.lua", --Prison Life
-    [606849621] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/Viper%20Hub%20Games/ViperHub_VehicleSimulator.lua" --Jail Break
+    [2534724415] = "", --ERLC
+    [155615604] = "", --Prison Life
+    [606849621] = "" --Jail Break
 
 }
 
