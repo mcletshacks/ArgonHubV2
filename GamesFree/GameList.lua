eb1724cb8f37aee93049d3c8698030d01e64c653
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2023-01-13 01:22:19 +0100

    Update GameList.lua

--- GamesFree/GameList.lua
+++ GamesFree/GameList.lua
@@ -1,10 +1,10 @@
 getgenv().Games = {
     [2534724415] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/Emergency%20Response%20Liberty%20County.lua", --ERLC
     [155615604] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/PrisonLife.lua", --Prison Life
-    [606849621] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/JailBreak.lua" --Jail Break
-    [9498006165] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/TappingSimulator.lua" --Tapping Simulator
-    [286090429] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/Arsenal.lua" --Arsenal
-    [6677985923] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/MillionaireEmpire.lua" --Millionaire Empire
+    [606849621] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/JailBreak.lua", --Jail Break
+    [9498006165] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/TappingSimulator.lua", --Tapping Simulator
+    [286090429] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/Arsenal.lua", --Arsenal
+    [6677985923] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/MillionaireEmpire.lua", --Millionaire Empire
 }
 
 getgenv().Unknown = {
