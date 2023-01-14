b4cbeda60927e9f4cab6b98fa9c839cc31ff3dd3
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2023-01-14 08:38:32 +0100

    Update GameList.lua

--- GamesFree/GameList.lua
+++ GamesFree/GameList.lua
@@ -5,6 +5,7 @@ getgenv().Games = {
     [9498006165] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/TappingSimulator.lua", --Tapping Simulator
     [286090429] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/Arsenal.lua", --Arsenal
     [6677985923] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/MillionaireEmpire.lua", --Millionaire Empire
+    [142823291] = "https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/GamesFree/MurderMystery2.lua" --Murder Mystery 2
 }
 
 getgenv().Unknown = {
