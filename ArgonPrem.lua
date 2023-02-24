149c45713dc192aa75cbba0d8ec0abcc45c924d7
mcletshacks <77382229+mcletshacks@users.noreply.github.com>
2023-02-24 21:04:11 +0100

    Update ArgonPrem.lua

--- ArgonPrem.lua
+++ ArgonPrem.lua
@@ -1,6 +1,7 @@
 --xd
 --it will be free for some time
---Loader loadstring (need to use this): loadstring(game:HttpGet("https://raw.githubusercontent.com/mcletshacks/ArgonHubV2/main/LoadPremium/Loader.lua", true))()
 --Argon Hub Dev Team
 
 game.Players.LocalPlayer:kick("Update ccversi..n")
+
+--bc
